05b284b3a239568743f00314d787025a0ad3e1ad
Justine Coulon <justinecoulon1@gmail.com>
2025-03-21 06:38:26 +0100

    added use client + removed log

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import styles from './page.module.css';
-import { useLocale, useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
   const t = useTranslations('header');
-  console.log(useLocale());
   return (
     <div className={styles.page}>
       <main className={styles.main}>{t('main-title')}</main>
