4908d86abe39af34b424e140a1c0a041256ac5bb
Justine Coulon <justinecoulon1@gmail.com>
2025-03-30 13:30:23 +0200

    added about page content

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,9 +1,26 @@
 'use client';
 
+import styles from './about-page.module.css';
+import { useTranslations } from 'next-intl';
+import { Link } from '@/i18n/routing';
+
 export default function About() {
+  const t = useTranslations('about');
+
   return (
-    <div>
-      <>About</>
+    <div className={styles.aboutPage}>
+      <div className={styles.aboutPageContainer}>
+        <div className={styles.aboutPageHeaderSection}></div>
+        <h2>{t('title')}</h2>
+        <div className={styles.aboutPagePresentationSection}>
+          <p>
+            {t.rich('presentation', {
+              br: () => <br />,
+              projects: (chunks) => <Link href="/projects">{chunks}</Link>,
+            })}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
