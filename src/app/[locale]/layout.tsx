bbe031815a54cead98022fdf227cf0742f8c99e3
Justine Coulon <justinecoulon1@gmail.com>
2025-03-30 16:23:34 +0200

    changed default theme to light

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <NextIntlClientProvider messages={messages}>
         <body className={athiti.variable}>
-          <ThemeProvider attribute={'data-theme'} defaultTheme={'system'} enableSystem>
+          <ThemeProvider attribute={'data-theme'} defaultTheme={'light'}>
             <Header />
             <main>{children}</main>
           </ThemeProvider>
